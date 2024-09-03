b2d4feaf3d95ba760b83c06aaf40476b169fdd4d
hyzhu <zhuhuiyu2499669698@163.com>
2024-09-03 19:43:49 +0800

    NodeJS学习的完善

--- NodeJS/内置模块/fs.js
+++ NodeJS/内置模块/fs.js
@@ -1,27 +1,38 @@
 const fs = require('fs')
 // 异步读取文件
-// fs.readFile('E:/Projects/frontend-study/NodeJS内置模块/test.txt'
+// fs.readFile('E:/Projects/frontend-study/NodeJS/内置模块/test.txt'
 //     ,'utf-8',
-//     (err, date) => {
-//         console.log('文件内容是：',date)
-//     }
-// )
+//     (err, data) => {
+//         if (err) {
+//             console.error('读取文件时出错:', err);
+//         } else {
+//             console.log('文件内容是:', data);
+//         }
+//     });
 
 // 同步写入文件
-try{
-    let content = '新的文件内内容'
-    fs.writeFileSync('E:/Projects/frontend-studyNodeJS/内置模块/test.txt',content
-    )
-}catch(err){
-    console.error(err)
-}
-
-// 检查文件状态
 // try{
-//     let status = fs.statSync('E:/Projects/frontend-study/NodeJS/内置模块/test.txt')
-//     console.log(status.isFile())
-//     console.log(status.isDirectory())
-//     console.log(status.size)
+//     let content = '新的文件内内容'
+//     fs.writeFileSync('E:/Projects/frontend-study/NodeJS/内置模块/test.txt',content
+//     )
 // }catch(err){
 //     console.error(err)
 // }
+
+// 检查文件状态
+try {
+    // 使用 fs.statSync 同步获取文件状态信息
+    let status = fs.statSync('E:/Projects/frontend-study/NodeJS/内置模块/test.txt');
+    
+    // 检查路径是否是文件
+    console.log(status.isFile());  // 输出: true 如果是文件
+    
+    // 检查路径是否是目录
+    console.log(status.isDirectory());  // 输出: true 如果是目录
+    
+    // 输出文件的大小（以字节为单位）
+    console.log(status.size);
+} catch (err) {
+    // 如果路径不存在或出现其他错误，则捕获并输出错误信息
+    console.error(err);
+}
---
+++ NodeJS/环境变量/index.js
@@ -0,0 +1,6 @@
+const { log } = require("console")
+
+process.env.baseURL= "http://api.xx.com"
+console.log(process.env.NODE_ENV)
+console.log(process.env.baseURL);
+
