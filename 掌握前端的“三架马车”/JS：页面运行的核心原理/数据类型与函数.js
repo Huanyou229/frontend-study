4cd294c97bc1e10b23c308cca024b6a704374e1e
hyzhu <zhuhuiyu2499669698@163.com>
2024-09-07 22:47:04 +0800

    前端三架马车的学习

---
+++ 掌握前端的“三架马车”/JS：页面运行的核心原理/数据类型与函数.js
@@ -0,0 +1,10 @@
+var a = "前端真好玩";
+var b = a;
+b = "前端真有趣";
+console.log("a:", a);
+console.log("b:", b);
+var a1 = { name: "前端⼈" };
+var b1 = a1;
+b1.name = "程序员";
+console.log("a1:", a1);
+console.log("b1:", b1);
